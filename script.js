dd0bd17650444e5d7ad2761783a16cc89c8e7f79
Robert Geica <geicarobert@gmail.com>
2019-09-28 22:11:10 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,216 @@
+// bosnia
+
+var bo = document.getElementById("bosnia-canvas");
+    var ctx = bo.getContext("2d");
+                        
+    ctx.beginPath();
+    ctx.moveTo(130, 0);
+    ctx.lineTo(130, 100);
+    ctx.lineTo(50, 0);
+    ctx.fillStyle = "#FECB00";
+                        
+    ctx.fill();
+
+
+// greece
+
+var gr = document.getElementById("greece-canvas");
+    var gtx = gr.getContext("2d");
+
+    gtx.beginPath();
+
+                   
+    gtx.fillStyle = "#0D5EAF";
+    gtx.fillRect(0,0, 150, 11);
+
+    gtx.fillStyle = "white";
+    gtx.fillRect(0,11, 150, 11);
+
+    gtx.fillStyle = "#0D5EAF";
+    gtx.fillRect(0,22, 150, 11);
+
+    gtx.fillStyle = "white";
+    gtx.fillRect(0,33, 150, 11);
+
+    gtx.fillStyle = "#0D5EAF";
+    gtx.fillRect(0,44, 150, 11);
+
+    gtx.fillStyle = "white";
+    gtx.fillRect(0,55, 150, 11);
+
+    gtx.fillStyle = "#0D5EAF";
+    gtx.fillRect(0,66, 150, 11);
+
+    gtx.fillStyle = "white";
+    gtx.fillRect(0,77, 150, 11);
+
+    gtx.fillStyle = "#0D5EAF";
+    gtx.fillRect(0,88, 150, 11);
+
+    gtx.moveTo(0, 0);
+    gtx.lineTo(50, 0);
+    gtx.lineTo(50, 50);
+    gtx.lineTo(0, 50);
+
+    gtx.moveTo(20,0);
+    gtx.lineTo(20,20);
+    gtx.lineTo(0,20);
+
+    gtx.lineTo(0,30);
+    gtx.lineTo(20,30);
+    gtx.lineTo(20,50);
+
+    gtx.lineTo(30, 50);
+    gtx.lineTo(30, 30);
+    gtx.lineTo(50, 30);
+
+    gtx.lineTo(50, 20);
+    gtx.lineTo(30, 20);
+    gtx.lineTo(30, 0);
+
+                    
+    gtx.fillStyle = "#fff";
+    gtx.fillRect(0,0, 50,50);
+
+    gtx.fillStyle = "#0D5EAF";
+
+    gtx.fill();
+
+// sweden
+var sw = document.getElementById("sweden-canvas");
+    var stx = sw.getContext("2d");
+
+
+    stx.moveTo(50, 0);
+
+    stx.lineTo(50, 40);
+    stx.lineTo(0, 40);
+    stx.lineTo(0, 60);
+
+
+    stx.lineTo(50, 60);
+    stx.lineTo(50, 100);
+    stx.lineTo(70, 100)
+
+    stx.lineTo(70, 60);
+    stx.lineTo(150, 60);
+    stx.lineTo(150, 40);
+
+    stx.lineTo(70, 40);
+    stx.lineTo(70, 0);
+    stx.lineTo(50, 0);
+
+    stx.fillStyle = "#FFCD00";
+    stx.fill();
+
+// switzerland
+
+var swi = document.getElementById("switz-canvas");
+    var swtx = swi.getContext("2d");
+
+    swtx.beginPath();
+
+    swtx.moveTo(65, 20);
+    swtx.lineTo(65, 40);
+
+    swtx.lineTo(45, 40);
+    swtx.lineTo(45, 60);
+    swtx.lineTo(65, 60);
+
+    swtx.lineTo(65, 80);
+    swtx.lineTo(85, 80);
+    swtx.lineTo(85, 60);
+
+    swtx.lineTo(105, 60);
+    swtx.lineTo(105, 40);
+    swtx.lineTo(85, 40);
+
+    swtx.lineTo(85, 20);
+    swtx.lineTo(65, 20);
+
+    swtx.fillStyle = "#fff";
+    swtx.fill();
+
+// england
+var en = document.getElementById("england-canvas");
+    var etx = en.getContext("2d");
+
+    etx.beginPath();
+
+    etx.moveTo(65, 0);
+
+    etx.lineTo(65, 40);
+    etx.lineTo(0, 40);
+    etx.lineTo(0, 60);
+
+
+    etx.lineTo(65, 60);
+    etx.lineTo(65, 100);
+    etx.lineTo(85, 100)
+
+    etx.lineTo(85, 60);
+    etx.lineTo(150, 60);
+    etx.lineTo(150, 40);
+
+    etx.lineTo(85, 40);
+    etx.lineTo(85, 0);
+    etx.lineTo(65, 0);
+
+    etx.fillStyle = "#CF081F";
+    etx.fill();
+
+// scotland
+var sc = document.getElementById("scotland-canvas");
+    var sctx = sc.getContext("2d");
+
+    sctx.beginPath();
+
+    sctx.moveTo(10, 0);
+
+    sctx.lineTo(150, 90);
+    sctx.lineTo(150, 100);
+    sctx.lineTo(140, 100);
+
+    sctx.lineTo(0, 10);
+    sctx.lineTo(0, 0);
+    sctx.lineTo(10, 0);
+
+    sctx.lineTo(140, 0);
+    sctx.lineTo(0, 90);
+    sctx.lineTo(0, 100);
+    sctx.lineTo(10, 100);
+
+    sctx.lineTo(150, 10);
+    sctx.lineTo( 150, 0);
+    sctx.lineTo(140, 0);        
+
+    sctx.fillStyle = "#fff";
+    sctx.fill();
+
+//nireland
+var n = document.getElementById("nireland-canvas");
+    var ntx = n.getContext("2d");
+
+    ntx.beginPath();
+
+    ntx.moveTo(10, 0);
+
+    ntx.lineTo(150, 90);
+    ntx.lineTo(150, 100);
+    ntx.lineTo(140, 100);
+
+    ntx.lineTo(0, 10);
+    ntx.lineTo(0, 0);
+    ntx.lineTo(10, 0);
+
+    ntx.lineTo(140, 0);
+    ntx.lineTo(0, 90);
+    ntx.lineTo(0, 100);
+    ntx.lineTo(10, 100);
+
+    ntx.lineTo(150, 10);
+    ntx.lineTo( 150, 0);
+    ntx.lineTo(140, 0);
+
+    ntx.fillStyle = "#B11E1A";
+    ntx.fill();
